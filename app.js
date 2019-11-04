43274394877a508695f8add6bedcb9082d51337e
Firas1993 <kebsi.firas@competitoor.com>
2019-11-04 22:51:43 +0100

    "connection to db"

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
+const nodemon = require('nodemon');
+require("dotenv/config");
 //Middlewares
+/*
 app.use('/posts',()=>{
     console.log('this is a midlewaez running');
 })
-
+*/
 //ROUTES
 app.get('/',(req,res)=>{
     res.send("<h1>We are on home</h1>");
@@ -13,7 +16,7 @@ app.get('/',(req,res)=>{
 })
 
 app.get("/posts", (req, res) => {
-  res.send("<h1>We are on post</h1>");
+  res.send("<h3>We are on post</h3>");
   //this excuting when we go to /
 });
 
@@ -23,3 +26,10 @@ app.get("/posts", (req, res) => {
 
 //HOW to we start listening to the server
 app.listen(3000); 
+
+// connstct to database
+mongoose.connect(
+  process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
+  console.warn("connected to db");
+});
+

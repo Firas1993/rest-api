f9f63fe093369271f633d275074a28c0e4cf6c62
Firas1993 <kebsi.firas@competitoor.com>
2019-11-04 12:20:38 +0100

    project configuration with fixing variable

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-const express = require('express');
+const app = require('express');
+
+//ROUTES
+app.get('/',(req,res)=>{
+    res.send('We are on home');
+//this excuting when we go to /
+})
+
+app.get("/posts", (req, res) => {
+  res.send("We are on post");
+  //this excuting when we go to /
+});
+
+// .post to post somthing
+// .patch to updating 
+// .delet to delete somthing
+
+//HOW to we start listening to the server
+app.listen(3000); 
